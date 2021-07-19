16f8b26f1ae3c2709b37c4e0c544e53a50e8a10b
Luis150228 <luis.rangeldiaz@gmail.com>
2021-07-19 18:14:38 -0500

    Se crea el todo list clase 108

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import {saludar} from './js/componentes';
+// import {Todo} from './classes/todo.class';
+// import {TodoList} from './classes/todo-list.class';
+import {Todo, TodoList} from './classes';
 import './styles.css';
 
-const nombre = 'Dafne';
+const todoList = new TodoList();
 
-saludar(nombre);
+const tarea = new Todo('Aprender Javascript');
+const tarea2 = new Todo('Comprar un Dragon');
+
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+
+console.log(todoList);
